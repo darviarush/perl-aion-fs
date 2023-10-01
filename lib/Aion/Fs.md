c43862fadb01d62974baf133783fe99fc41af02b
Yaroslav O. Kosmina <dart@cpan.org>
2023-10-01 22:54:35 +0300

    Make function cat and lay

--- lib/Aion/Fs.md
+++ lib/Aion/Fs.md
@@ -11,51 +11,157 @@ Aion::Fs - utilities for filesystem: read, write, find, replace files, etc
 ```perl
 use Aion::Fs;
 
-my $aion_fs = Aion::Fs->new();
+lay mkpath "hello/world.txt", "hi!";
+lay mkpath "hello/big/world.txt", "hellow!";
 
-$aion_fs  # -> 1
+my $count = replace { s/h/${\ PATH} H/ }
+    find "hello", "-f", "**.txt",
+        noenter { PATH =~ wildcard "*small*" };
+
+$count # -> 2
+
+cat "hello/world.txt"       # => hello/world.txt Hi!
+cat "hello/big/world.txt"   # => hello/big/world.txt Hellow!
+
+scalar find "hello"  # -> 4
+
+[find "hello", "*.txt"]  # -> [qw!  hello/world.txt  hello/big/world.txt  !]
+[find "hello", "-d"]  # -> [qw!  hello  hello/big  !]
+
+erase reverse find "hello";
 ```
 
 # DESCRIPTION
 
-Aion::Fs — .
+
 
 # SUBROUTINES/METHODS
 
-## 
+## PATH ()
 
 .
 
 ```perl
-my $aion_fs = Aion::Fs->new();
-
-$aion_fs  # -> 1
+my $aion_fs = Aion::Fs->new;
+$aion_fs->PATH  # -> .3
 ```
 
-# INSTALL
+## cat ($file)
 
-Add to **cpanfile** in your project:
+{use open IN => ':raw'; cat }
 
-```cpanfile
-requires 'Aion::Fs',
-    git => 'https://github.com/darviarush/perl-aion-fs.git',
-    ref => 'master',
-;
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->cat($file)  # -> .3
 ```
 
-And run command:
+## lay ($file, $content)
 
-```sh
-$ sudo cpm install -gvv
+{use open OUT => ':raw'; lay $path }
+
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->lay($file, $s)  # -> .3
+```
+
+## find ()
+
+Найти файлы
+
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->find  # -> .3
+```
+
+## erase ()
+
+Стирает все указанные файлы. Возвращает переданные файлы
+
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->erase  # -> .3
+```
+
+## noenter ()
+
+Не входить в подкаталоги
+
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->noenter  # -> .3
+```
+
+
+## mkpath ($path, $mode)
+
+как mkdir -p
+
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->mkpath($path, $mode)  # -> .3
+```
+
+## mtime ($file)
+
+Вернуть время модификации файла
+
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->mtime  # -> .3
+```
+
+## replace (&sub, @files)
+
+Производит замену во всех указанных файлах. Возвращает количество файлов в которых были замены
+
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->replace  # -> .3
+```
+
+## include ($pkg)
+
+Подключает модуль, если он ещё не подключён, и возвращает его
+
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->include($pkg)  # -> .3
+```
+
+## require_file ($file)
+
+.
+
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->require_file($file)  # -> .3
+```
+
+## wildcard ($wildcard)
+
+Переводит вилдкард в регулярку
+
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->wildcard($wildcard)  # -> .3
+```
+
+## goto_editor ($path, $line)
+
+.
+
+```perl
+my $aion_fs = Aion::Fs->new;
+$aion_fs->goto_editor($path, $line)  # -> .3
 ```
 
 # AUTHOR
 
 Yaroslav O. Kosmina [dart@cpan.org](mailto:dart@cpan.org)
 
-# COPYRIGHT AND LICENSE
-This software is copyright (c) 2023 by Yaroslav O. Kosmina.
-
-This is free software; you can redistribute it and/or modify it under the same terms as the Perl 5 programming language system itself.
+# LICENSE
 
 ⚖ **GPLv3**
+
+# COPYRIGHT
+The Aion::Fs is copyright © 2023 by Yaroslav O. Kosmina. Rusland. All rights reserved.
--- lib/Aion/Fs.pm
+++ lib/Aion/Fs.pm
@@ -5,6 +5,8 @@ use common::sense;
 
 our $VERSION = "0.0.0-prealpha";
 
+use List::Util qw/any all/;
+
 require Exporter;
 our @EXPORT = our @EXPORT_OK = grep {
 	*{$Aion::Fs::{$_}}{CODE} && !/^(_|(NaN|import)\z)/n
@@ -20,85 +22,161 @@ sub include($) {
 	$pkg
 }
 
-# как mkdir -p
-sub mkpath ($;$) {
-	my ($path, $mode) = @_;
-	$mode //= 0755;
-	mkdir $`, $mode while $path =~ m!/!g;
-	$path
-}
-
 # считать файл, если он ещё не был считан
 our %FILE_INC;
 sub require_file ($) {
 	my ($file) = @_;
 	return undef if exists $FILE_INC{$file};
-	my $x = read_file($file);
+	my $x = cat($file);
 	$FILE_INC{$file} = 1;
 	$x
 }
 
-BEGIN {
+# Текущий путь в exec и replace
+our $PATH;
+sub PATH() {$PATH}
 
-# Считывает файл в указанной кодировке
-sub read(@) {
-    my ($file, $layer) = @_;
-	$layer //= ":utf8";
-	open my $f, "<$layer", $file or die "read $file: $!";
+# как mkdir -p
+sub mkpath (;$$) {
+	my ($path, $mode) = @_;
+	$path //= $PATH;
+	$mode //= 0755;
+	mkdir $`, $mode while $path =~ m!/!g;
+	$path
+}
+
+# Считывает файл
+# Для считываения :raw:
+#  {use open IN => ':raw'; cat }
+sub cat(;$) {
+    my ($file) = @_;
+	$file //= $PATH;
+	open my $f, "<", $file or die "cat $file: $!";
 	read $f, my $x, -s $f;
 	close $f;
 	$x
 }
 
 # записать файл
-sub write (@) {
-	my ($file, $s, $layer) = @_;
-	$layer //= ":utf8";
-	open my $f, ">$layer", $file or die "> $file: $!";
+# Для записи :raw:
+#  {use open OUT => ':raw'; lay $path }
+sub lay (;$$) {
+	my ($file, $s) = @_;
+	$file //= $PATH;
+	$s //= $_;
+	open my $f, ">", $file or die "lay $file: $!";
 	print $f $s;
 	close $f;
-	wantarray? ($file, $s, $layer): $file
-}
-
-}
-
-# Вернуть время модификации файла
-sub mtime($) {
-	(stat shift())[9]
+	$file
 }
 
 # Вернуть время модификации файла
+sub mtime(;$) {
+	my $file = shift // $PATH;
+	(stat $file)[9] // die "mtime $file: $!"
+}
+
+# Найти файлы
 sub find($;@) {
-	my ($file, @filters) = @_;
-    my @S = ref $file? @$file: $file;
-    
+	my $file = shift;
+    $file = [$file] unless ref $file;
+
+	my @noenter;
+	my @filters;
+	for(@_) {
+		push @noenter, $_ when ref $_ eq "Aion::Fs::noenter";
+		push @filters, $_ when ref $_ eq "CODE";
+		push @filters, do { my $re = $_; sub { $PATH =~ $re } } when ref $_ eq "Regexp";
+		push @filters, eval(join "", "sub { ", (join " && ", map "-$_", split //, $1), " }") || die when /^-([a-z]+)$/;
+		default { push @filters, wildcard($_) }
+	}
+
+	my @ret;
+
+    FILE: while(@$file) {
+		local $PATH = shift @$file;
+
+		push @ret, $PATH if all { $_->() } @filters;
+
+		if(-d $PATH) {
+			next FILE if any { $_->() } @noenter;
+
+			opendir my $dir, $PATH or die "find $PATH: $!";
+			while(readdir $dir) {
+				push @$file, "$PATH/$_";
+			}
+			closedir $dir;
+		}
+	}
+
+	@ret
 }
 
-# 
+# Не входить в подкаталоги
 sub noenter(&@) {
-
+	bless(shift, "Aion::Fs::noenter"), @_
 }
 
-# 
-sub exec(&@) {
+# # 
+# sub offset($@) {
+# 	bless(shift, "Aion::Fs::offset"), @_
+# }
 
-}
+# # 
+# sub limit($@) {
+# 	bless(shift, "Aion::Fs::limit"), @_
+# }
 
-# Производит замену во всех файлах
+# Производит замену во всех указанных файлах. Возвращает количество файлов в которых были замены
 sub replace(&@) {
     my $fn = shift;
-    local $_;
+	my $count = 0; local($_, $PATH);
     for my $path (@_) {
-        my $file = $_ = read $path;
-        $fn->($path);
-        write $path, $_ if $file ne $_;
+		$PATH = $path;
+        my $file = $_ = cat;
+        $fn->();
+        $count++, lay if $file ne $_;
     }
+	$count
+}
+
+# Стирает все указанные файлы. Возвращает переданные файлы
+sub erase(@) {
+    -d? rmdir: unlink or die "erase ${\(-d? 'dir': 'file')} $_: $!" for @_;
+	@_
+}
+
+# Переводит вилдкард в регулярку
+sub wildcard(;$) {
+	my ($wildcard) = @_;
+	$wildcard = $_;
+	$wildcard =~ s{
+		(?<file> \*\*)
+		| (?<path> \*)
+		| (?<anyn> \?\? )
+		| (?<any> \? )
+		| (?<w1> \{ )
+		| (?<w2> \} )
+		| (?<comma> , )
+		| .
+	}{
+		exists $+{file}? "[^/]*?":
+		exists $+{path}? ".*?":
+		exists $+{anyn}? "[^/]":
+		exists $+{any}? ".":
+		exists $+{w1}? "(":
+		exists $+{w2}? ")":
+		exists $+{comma}? "|":
+		quotemeta $&
+	}gxe;
+	qr/$wildcard/s
 }
 
 # Открывает файл на указанной строке в редакторе
+use config EDITOR => "vscodium";
 sub goto_editor($$) {
 	my ($path, $line) = @_;
-	my $p = $main_config::editor;
+	my $p = EDITOR;
 	$p =~ s!%p!$path!;
 	$p =~ s!%l!$line!;
 	my $status = system $p;
